f0c2c792876d9b72467a47a2c7ee08f41656727c
Pavlin Petkov <fihercho@gmail.com>
2026-03-11 22:29:20 +0200

    new design

--- src/components/Reviews/ReviewsView.jsx
+++ src/components/Reviews/ReviewsView.jsx
@@ -1,12 +1,13 @@
 import { SectionHeader } from '../atoms/SectionHeader'
 import { RatingBadge } from './RatingBadge'
 import { ReviewsCarousel } from './ReviewsCarousel'
+import bgImage from '../../assets/images/gallery/evapostedit3.jpg'
 
 export const ReviewsView = ({
   sectionTag, title, googleBtn, note, googleUrl,
   rating, totalCount, reviews, lang
 }) => (
-  <section id="reviews" className="reviews">
+  <section id="reviews" className="reviews reviews--bg" style={{ backgroundImage: `url(${bgImage})` }}>
     <div className="container">
       <SectionHeader tag={sectionTag} title={title} />
       <RatingBadge rating={rating} totalCount={totalCount} profileUrl={googleUrl} />
